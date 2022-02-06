cf6f9eca2bf37bf0b863ff188b782da54602735d
Stephanie Fernandes <msfern.dev@gmail.com>
2022-02-06 14:28:37 -0300

    feat: :heavy_plus_sign: add support for CSS Modules on .tsx files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,7 @@
 module.exports = {
-    siteMetadata: {
-      title: `Learning Gatsby`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+  siteMetadata: {
+    title: `Learning Gatsby`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: ["gatsby-plugin-dts-css-modules"],
+};
